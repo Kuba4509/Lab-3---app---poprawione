9b1247df1934d00031430adcd2f62463f7b62272
Jakub Nowak <jakub.nowak@microsoft.wsei.edu.pl>
2024-01-22 18:54:34 +0100

    Add project files.

---
+++ Lab 3 - app/Controllers/TravelController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Lab_3___app.Models;
+
+namespace Labolatorium3___app.Controllers
+{
+    public class TravelController : Controller
+    {
+        static Dictionary<int, Travel> _travels = new Dictionary<int, Travel>();
+        static int id = 1;
+
+        public IActionResult Index()
+        {
+            return View(_travels);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Travel model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.Id = id++;
+                _travels.Add(model.Id, model);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            return View(_travels[id]);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Travel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _travels[model.Id] = model;
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            return View(_travels[id]);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Travel model)
+        {
+            _travels.Remove(model.Id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            return View(_travels[id]);
+        }
+    }
+}
---
+++ Lab 3 - app/Models/Travel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_3___app.Models
+{
+    public class Travel
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string StartingLocation { get; set; }
+        public string EndingLocation { get; set; }
+        public string Participants { get; set; }
+        public string Guide { get; set; }
+    }
+}
